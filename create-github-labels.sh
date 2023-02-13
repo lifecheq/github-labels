d907f8a83cba7157b8bb8e9b3f496f9e2a5a1268
Daniel Lebrero Berna <dlebrero@gmail.com>
2023-02-13 18:34:10 +0100

    Update labels

--- create-github-labels.sh
+++ create-github-labels.sh
@@ -8,6 +8,7 @@
 #
 # Silent, if $GITHUB_TOKEN is set in environment and repository provided as an argument:
 # ./create-github-labels myorg/myrepo
+# gh repo list lifecheq --json "name"  | jq ".[].name"  | cut -f 2 -d\" | awk '{print "lifecheq/" $0}'
 #
 # shellcheck disable=SC2155
 
@@ -19,28 +20,19 @@ DELETE_EXISTING_LABELS="${DELETE_EXISTING_LABELS:-1}"
 # Array of labels to create. If DELETE_EXISTING_LABELS=1, the labels list will
 # be exactly as below, otherwise labels below will be added to existing ones.
 LABELS=(
-  "CONFLICT"            "bc143e"  "Pull request has a conflict that needs to be resolved before it can be merged"
-  "DO NOT MERGE"        "d93f0b"  "Do not merge this pull request"
-  "Do not review"       "d93f0b"  "Do not review this pull request"
-  "Needs review"        "5319e7"  "Pull requests needs a review from assigned developers"
-  "Questions"           "b5f492"  "Pull requests has some questions that need to be answered before further review can progress"
-  "Ready for test"      "0e8a16"  "Pull request is ready for manual testing"
-  "Ready to be merged"  "c2e0c6"  "Pull request is ready to be merged (assigned after testing is complete)"
-  "Requires more work"  "b60205"  "Pull request was reviewed and reviver(s) asked to work further on the pull request"
-  "URGENT"              "d93f0b"  "Pull request needs to be urgently reviewed"
-
-  # Uncomment default Github labels below to preserve them.
-  # "bug"                 "d73a4a"  "Something isn't working"
-  # "duplicate"           "cfd3d7"  "This issue or pull request already exists"
-  # "enhancement"         "a2eeef"  "New feature or request"
-  # "help wanted"         "008672"  "Extra attention is needed"
-  # "good first issue"    "7057ff"  "Good for newcomers"
-  # "invalid"             "e4e669o" "This doesn't seem right"
-  # "question"            "d876e3"  "Further information is requested"
-  # "wontfix"             "ffffff"  "This will not be worked on"
+  "Feature Flag? No" "B60205" "The change is not behind a feature flag, experiment, dark launch or similar"
+  "Feature Flag? Yes" "0E8A16" "The change is behind a feature flag, experiment, dark launch or similar"
+  "Risk: high" "E99695" "High Risk change"
+  "Risk: low" "C2E0C6" "Low Risk change"
+  "Risk: medium" "FEF2C0" "Medium Risk change"
 )
 
-main(){
+## Labels that we will not delete
+LABELS_TO_KEEP=(
+  "adr" "FEE8DB" "To be able to exclude ADR PR from Github pullreminders"
+)
+
+main() {
   echo
   if [ "${DELETE_EXISTING_LABELS}" == "1" ]; then
     echo "This script will remove the default GitHub labels."
@@ -51,7 +43,7 @@ main(){
   echo "A personal access token is required to access private repositories."
   echo
 
-  if [  "${REPO}" == "" ]; then
+  if [ "${REPO}" == "" ]; then
     echo ''
     echo -n 'GitHub Org/Repo (e.g. foo/bar): '
     read -r REPO
@@ -59,7 +51,7 @@ main(){
     REPO="$1"
   fi
 
-  if [  "${GITHUB_TOKEN}" == "" ]; then
+  if [ "${GITHUB_TOKEN}" == "" ]; then
     echo ''
     echo -n 'GitHub Personal Access Token: '
     read -r -s GITHUB_TOKEN
@@ -83,7 +75,7 @@ main(){
     echo "Checking existing labels"
     existing_labels_strings="$(label_all)"
     # shellcheck disable=SC2207
-    IFS=$'\n' existing_labels=( $(xargs -n1 <<<"${existing_labels_strings}") )
+    IFS=$'\n' existing_labels=($(xargs -n1 <<<"${existing_labels_strings}"))
     for existing_label_name in "${existing_labels[@]}"; do
       if ! is_provided_label "${existing_label_name}"; then
         echo "Removing label \"${existing_label_name}\" as it is not in thr provided list"
@@ -98,9 +90,9 @@ main(){
 
   count=0
   for value in "${LABELS[@]}"; do
-    if (( count % 3 == 0)); then
+    if ((count % 3 == 0)); then
       name="${value}"
-    elif (( count % 3 == 1)); then
+    elif ((count % 3 == 1)); then
       color="${value}"
     else
       description="${value}"
@@ -121,7 +113,7 @@ main(){
       fi
 
     fi
-    count=$(( count + 1 ))
+    count=$((count + 1))
   done
 
   echo
@@ -129,43 +121,54 @@ main(){
   echo
 }
 
-is_provided_label(){
+is_provided_label() {
   label="${1}"
 
   count=0
   for value in "${LABELS[@]}"; do
-    if (( count % 3 == 0)); then
+    if ((count % 3 == 0)); then
       name="${value}"
       if [ "${label}" == "${name}" ]; then
-        return 0;
+        return 0
       fi
     fi
-    count=$(( count + 1 ))
+    count=$((count + 1))
+  done
+
+  count=0
+  for value in "${LABELS_TO_KEEP[@]}"; do
+    if ((count % 3 == 0)); then
+      name="${value}"
+      if [ "${label}" == "${name}" ]; then
+        return 0
+      fi
+    fi
+    count=$((count + 1))
   done
 
   return 1
 }
 
-user_has_access(){
-  status=$( \
+user_has_access() {
+  status=$(
     curl -s -I \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    --include -H "Accept: application/vnd.github.symmetra-preview+json" \
-    -o /dev/null \
-    -w "%{http_code}" \
-    --request GET \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels" \
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      --include -H "Accept: application/vnd.github.symmetra-preview+json" \
+      -o /dev/null \
+      -w "%{http_code}" \
+      --request GET \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels"
   )
   [ "${status}" == "200" ]
 }
 
-label_all(){
-  response=$( \
+label_all() {
+  response=$(
     curl -s \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    --include -H "Accept: application/vnd.github.symmetra-preview+json" \
-    --request GET \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels" \
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      --include -H "Accept: application/vnd.github.symmetra-preview+json" \
+      --request GET \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels"
   )
   jsonval "${response}" "name"
 }
@@ -173,88 +176,92 @@ label_all(){
 label_exists() {
   local name="${1}"
   local name_encoded=$(uriencode "${name}")
-  status=$( \
+  status=$(
     curl -s -I \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    --include -H "Accept: application/vnd.github.symmetra-preview+json" \
-    -o /dev/null \
-    -w "%{http_code}" \
-    --request GET \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}" \
-    )
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      --include -H "Accept: application/vnd.github.symmetra-preview+json" \
+      -o /dev/null \
+      -w "%{http_code}" \
+      --request GET \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}"
+  )
   [ "${status}" == "200" ]
 }
 
-label_create(){
+label_create() {
   local name="${1}"
   local color="${2}"
   local description="${3}"
-  local status=$(curl -s \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    -H "Accept: application/vnd.github.symmetra-preview+json" \
-    -o /dev/null \
-    -w "%{http_code}" \
-    --request POST \
-    --data "{\"name\":\"${name}\",\"color\":\"${color}\", \"description\":\"${description}\"}" \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels" \
+  local status=$(
+    curl -s \
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      -H "Accept: application/vnd.github.symmetra-preview+json" \
+      -o /dev/null \
+      -w "%{http_code}" \
+      --request POST \
+      --data "{\"name\":\"${name}\",\"color\":\"${color}\", \"description\":\"${description}\"}" \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels"
   )
   [ "${status}" == "201" ]
 }
 
-label_update(){
+label_update() {
   local name="${1}"
   local color="${2}"
   local description="${3}"
   local name_encoded=$(uriencode "${name}")
-  local status=$(curl -s \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    -H "Accept: application/vnd.github.symmetra-preview+json" \
-    -o /dev/null \
-    -w "%{http_code}" \
-    --request PATCH \
-    --data "{\"name\":\"${name}\",\"color\":\"${color}\", \"description\":\"${description}\"}" \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}" \
+  local status=$(
+    curl -s \
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      -H "Accept: application/vnd.github.symmetra-preview+json" \
+      -o /dev/null \
+      -w "%{http_code}" \
+      --request PATCH \
+      --data "{\"name\":\"${name}\",\"color\":\"${color}\", \"description\":\"${description}\"}" \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}"
   )
   [ "${status}" == "200" ]
 }
 
-label_delete(){
+label_delete() {
   local name="${1}"
   local color="${2}"
   local description="${3}"
   local name_encoded=$(uriencode "${name}")
-  local status=$(curl -s \
-    -u "${GITHUB_TOKEN}":x-oauth-basic \
-    -H "Accept: application/vnd.github.symmetra-preview+json" \
-    -o /dev/null \
-    -w "%{http_code}" \
-    --request DELETE \
-    "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}" \
+  local status=$(
+    curl -s \
+      -u "${GITHUB_TOKEN}":x-oauth-basic \
+      -H "Accept: application/vnd.github.symmetra-preview+json" \
+      -o /dev/null \
+      -w "%{http_code}" \
+      --request DELETE \
+      "https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/labels/${name_encoded}"
   )
   [ "${status}" == "204" ]
 }
 
-jsonval(){
+jsonval() {
   local json="${1}"
   local prop="${2}"
 
-  temp=$(echo "${json}" \
-    | sed 's/\\\\\//\//g' \
-    | sed 's/[{}]//g'    \
-    | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
-    | sed 's/\"\:\"/\|/g' \
-    | sed 's/[\,]/ /g' \
-    | grep -w "${prop}" \
-    | cut -d":" -f2 \
-    | sed -e 's/^ *//g' -e 's/ *$//g' \
+  temp=$(
+    echo "${json}" |
+      sed 's/\\\\\//\//g' |
+      sed 's/[{}]//g' |
+      awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |
+      sed 's/\"\:\"/\|/g' |
+      sed 's/[\,]/ /g' |
+      grep -w "${prop}" |
+      cut -d":" -f2 |
+      sed -e 's/^ *//g' -e 's/ *$//g'
   )
   temp="${temp//${prop}|/}"
-  temp="$(echo "${temp}"|tr '\r\n' ' ')"
+  temp="$(echo "${temp}" | tr '\r\n' ' ')"
 
   echo "${temp}"
 }
 
-uriencode(){
+uriencode() {
   s="${1//'%'/%25}"
   s="${s//' '/%20}"
   s="${s//'"'/%22}"
@@ -274,14 +281,15 @@ uriencode(){
   printf %s "$s"
 }
 
-timeout(){
+timeout() {
   local seconds=${1}
   while [ "${seconds}" -gt 0 ]; do
-     echo -ne "Processing will start in $seconds seconds. Press Ctrl+C to abort\033[0K\r"
-     sleep 1
-     : $((seconds--))
+    echo -ne "Processing will start in $seconds seconds. Press Ctrl+C to abort\033[0K\r"
+    sleep 1
+    : $((seconds--))
   done
   echo
 }
 
 main "$@"
+
